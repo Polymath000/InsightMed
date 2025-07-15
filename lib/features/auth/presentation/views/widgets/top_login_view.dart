f0d75f50e0e6927566132bb789502064833ee76b
Mohamed Ehab <mehab1638@gmail.com>
2025-07-15 17:16:43 +0300

    patient information done

--- lib/features/auth/presentation/views/widgets/top_login_view.dart
+++ lib/features/auth/presentation/views/widgets/top_login_view.dart
@@ -3,30 +3,23 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../../core/utls/app_images.dart';
 
 class TopLoginView extends StatelessWidget {
-  const TopLoginView({
-    super.key,
-  });
+  const TopLoginView({super.key});
 
   @override
   Widget build(final BuildContext context) => Column(
-      children: [
-        const Image(image: AssetImage(Assets.assetsImagesLogo)),
-        SizedBox(height: MediaQuery.of(context).size.height / 30),
-        Text(
-          'Welcome',
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 24.sp,
-          ),
-        ),
-        SizedBox(height: MediaQuery.of(context).size.height / 85),
-        Text(
-          'Sign in to continue',
-          style: TextStyle(
-            fontWeight: FontWeight.w400,
-            fontSize: 16.sp,
-          ),
-        ),
-      ],
-    );
+    children: [
+      SizedBox(height: MediaQuery.of(context).size.height / 50),
+      const Image(image: AssetImage(Assets.assetsImagesLogo)),
+      SizedBox(height: MediaQuery.of(context).size.height / 125),
+      Text(
+        'Welcome',
+        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24.sp),
+      ),
+      SizedBox(height: MediaQuery.of(context).size.height / 85),
+      Text(
+        'Sign in to continue',
+        style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16.sp),
+      ),
+    ],
+  );
 }
