fe997eed8de8c11f8b5042fca12aecb726c5a736
Yousef Saber <yousef.saber128@gmail.com>
2025-07-26 09:59:00 +0300

    refactor error handling in Dio interceptor; streamline logging and improve readability

--- lib/core/helpers/dio_error_message.dart
+++ lib/core/helpers/dio_error_message.dart
@@ -1,21 +1,28 @@
-import 'package:dio/dio.dart' as dio_package;
+import 'package:dio/dio.dart';
 
 import 'get_auth_message.dart';
 
-String mapDioErrorToMessage(final dio_package.DioException e) {
+String mapDioErrorToMessage(final DioException e) {
   final messages = getAuthMessages;
   switch (e.type) {
-    case dio_package.DioExceptionType.connectionTimeout:
-    case dio_package.DioExceptionType.sendTimeout:
-    case dio_package.DioExceptionType.receiveTimeout:
-      return 'Oops! The connection took too long. Please check your internet and try again.';
-    case dio_package.DioExceptionType.badResponse:
-      return 'Sorry, we had trouble processing your request. Please try again in a moment.';
-    case dio_package.DioExceptionType.cancel:
+    case DioExceptionType.connectionTimeout:
+    case DioExceptionType.sendTimeout:
+    case DioExceptionType.receiveTimeout:
+      return 'Oops! The connection took too long. '
+          'Please check your internet and try again.';
+    case DioExceptionType.badResponse:
+      return 'Sorry, we had trouble processing your '
+          'request. Please try again in a moment.';
+    case DioExceptionType.cancel:
       return 'No worries, your request was cancelled.';
-    case dio_package.DioExceptionType.unknown:
-      return 'Something went wrong with the network. Please check your connection and try again.';
-    default:
-      return 'Unexpected error occurred. Please try again or contact support if the problem continues.';
+    case DioExceptionType.unknown:
+      return 'Something went wrong with the network. '
+          'Please check your connection and try again.';
+    case DioExceptionType.connectionError:
+      return 'Oops! Something went wrong with the network. '
+          'Please check your connection and try again.';
+    case DioExceptionType.badCertificate:
+      return 'Oops! Something went wrong with the network. '
+          'Please check your connection and try again.';
   }
 }
--- lib/core/network/interceptors.dart
+++ lib/core/network/interceptors.dart
@@ -3,31 +3,36 @@ import 'package:logger/logger.dart';
 
 /// This interceptor is used to show request and response logs
 class LoggerInterceptor extends Interceptor {
-  Logger logger = Logger(
-    printer: PrettyPrinter(methodCount: 0, colors: true, printEmojis: true),
-  );
+  Logger logger = Logger(printer: PrettyPrinter(methodCount: 0));
 
   @override
-  void onError(DioException err, ErrorInterceptorHandler handler) {
+  void onError(final DioException err, final ErrorInterceptorHandler handler) {
     final options = err.requestOptions;
     final requestPath = '${options.baseUrl}${options.path}';
-    logger.e('${options.method} request ==> $requestPath'); //Error log
-    logger.d(
-      'Error type: ${err.error} \n '
-      'Error message: ${err.message}',
-    ); //Debug log
+    logger
+      ..e('${options.method} request ==> $requestPath') //Error log
+      ..d(
+        'Error type: ${err.error} \n '
+        'Error message: ${err.message}',
+      ); //Debug log
     handler.next(err); //Continue with the Error
   }
 
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+  void onRequest(
+    final RequestOptions options,
+    final RequestInterceptorHandler handler,
+  ) {
     final requestPath = '${options.baseUrl}${options.path}';
     logger.i('${options.method} request ==> $requestPath'); //Info log
     handler.next(options); // continue with the Request
   }
 
   @override
-  void onResponse(Response response, ResponseInterceptorHandler handler) {
+  void onResponse(
+    final Response response,
+    final ResponseInterceptorHandler handler,
+  ) {
     logger.d(
       'STATUSCODE: ${response.statusCode} \n '
       'STATUSMESSAGE: ${response.statusMessage} \n'
