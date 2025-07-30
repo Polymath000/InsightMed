1c98c17d2e8f20cb84660b3774e61ae196342431
Abdelrahman Khaled <112812365+abdelrahman6644@users.noreply.github.com>
2025-07-30 17:30:24 +0300

    show upload image

--- lib/features/patient_rays_submission/presentation/views/widgets/upload_widget.dart
+++ lib/features/patient_rays_submission/presentation/views/widgets/upload_widget.dart
@@ -1,5 +1,6 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../../../../core/utls/themes/app_colors.dart';
 
@@ -12,40 +13,47 @@ class UploadWidget extends StatefulWidget {
 }
 
 class _UploadWidgetState extends State<UploadWidget> {
+  FilePickerResult? image;
+
   @override
   Widget build(final BuildContext context) => GestureDetector(
     onTap: () async {
-      final image = await FilePicker.platform.pickFiles();
-      if (image != null) {
-        final file = image.files.first;
+      image = await FilePicker.platform.pickFiles(withData: true);
+      if (image != null && image!.files.first.bytes != null) {
+        final file = image!.files.first;
         final imagePath = file.path;
         setState(() {
-          widget.onChangImagePath!(imagePath);
+          widget.onChangImagePath?.call(imagePath);
         });
       }
     },
-    child: const Card.filled(
-      child: SizedBox(
-        width: double.infinity,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 28),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.folder, size: 48, color: AppColors.grey),
-              Text(
-                'Drag & Drop or Browse Files',
-                style: TextStyle(fontSize: 16),
+    child: image != null && image!.files.first.bytes != null
+        ? Image.memory(
+            Uint8List.fromList(image!.files.first.bytes!),
+            height: 200,
+          )
+        : const Card.filled(
+            child: SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 28),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.folder, size: 48, color: AppColors.grey),
+                    Text(
+                      'Drag & Drop or Browse Files',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Browse Files',
+                      style: TextStyle(color: AppColors.blue, fontSize: 16),
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(height: 8),
-              Text(
-                'Browse Files',
-                style: TextStyle(color: AppColors.blue, fontSize: 16),
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-    ),
   );
 }
